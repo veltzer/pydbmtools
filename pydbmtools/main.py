1a2de71a88d23b9516f95698af214dd518b0ae23
Mark Veltzer <mark.veltzer@gmail.com>
2022-09-17 15:24:16 +0300

    dump now works

--- pydbmtools/main.py
+++ pydbmtools/main.py
@@ -2,17 +2,29 @@
 main entry point to the program
 """
 
+import dbm.gnu
 import pylogconf.core
-from pytconf import register_main, config_arg_parse_and_launch, register_endpoint
+from pytconf import register_main, config_arg_parse_and_launch, register_endpoint, get_free_args
 from pydbmtools.static import DESCRIPTION, APP_NAME, VERSION_STR
 
 
 @register_endpoint(
     description="List the content of a dbm file",
-    configs=[]
+    configs=[],
+    allow_free_args=True,
+    max_free_args=2,
 )
 def dump() -> None:
-    pass
+    filename = get_free_args()[0]
+    with dbm.gnu.open(filename) as db:
+        key = db.firstkey()
+        while key is not None:
+            value = db.get(key)
+            if value is not None:
+                print(key.decode(), len(value), type(value))
+            else:
+                print(key.decode(), type(value))
+            key = db.nextkey(key)
 
 
 @register_main(
